6a84fd232a858adea388029e170ace2dbe7f5f63
Mike <michael_eric_moore@hotmail.com>
2021-01-14 15:02:07 -0600

    add missing default export to types

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,3 @@
 export declare function awaitCb<T extends any[]>(fn: (cb: (...args: T) => any) => any): Promise<T>;
 export declare function awaitErrCb<T extends any[]>(fn: (cb: (err: any, ...args: T) => any) => any): Promise<T>;
+export default awaitCb;
